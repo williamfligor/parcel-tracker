b9d80d5634595db5a3135b8e63d63477a9261199
William Fligor <will@liveify.com>
2016-12-27 20:44:56 -0500

    add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+        name = 'parcel-tracker',
+        packages = ['parcel-tracker'], # this must be the same as the name above
+        version = '0.0.1',
+        description = 'Python Library for Package Tracking',
+        author = 'William Fligor',
+        author_email = 'will@fligor.us',
+        url = 'https://github.com/williamfligor/parcel-tracker', # use the URL to the github repo
+        keywords = ['parcel', 'tracking'], # arbitrary keywords
+        classifiers = [],
+        )
